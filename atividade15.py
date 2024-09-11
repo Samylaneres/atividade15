3ea7988f02cdbb370e457c5bb4119a53f1eccb3b
Samylaneres <samyaneres13@gmail.com>
2024-09-11 15:36:32 -0300

    subtração

---
+++ atividade15.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba dois números e uma operação (soma, subtração, multiplicação ou divisão) e execute a operação desejada.
+
+numero1 = float(input("Digite o primeiro numero: "))
+numero2 = float(input("Digite o segundo numero: "))
+
+resultado = numero1 - numero2 
+print("Resultado: ", resultado) 
+
